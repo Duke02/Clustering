cd3999e34a4ca49e48df8b3eb5363936a279219f
Trystan May <trystanmay@gmail.com>
2018-10-12 12:38:57 -0500

    Added more comments to code

--- main.py
+++ main.py
@@ -36,15 +36,24 @@ def createData(N = 100):
 def plot(data, centroids=[], labels=[], colors=[]):
 	# clear the current frame
 	plt.clf()
+	# If we weren't given any labels or colors
 	if len(labels) == 0 or len(colors) == 0:
+		# Let our colors be a gray
 		_colors = [["#999999"]]
+		# And our labels be all 0's
 		_labels = [0 for x in range(len(data))]
 	else:
+		# Otherwise use the given data.
 		_colors = colors
 		_labels = labels
+	# For all of our data
 	for i in range(len(data)):
 		# scatter the points
+		# and color them according to their labels.
+		# since labels will have values from [0, k) where
+		# k is the number of clusters
 		plt.scatter(data[i,0], data[i,1], c=_colors[:,int(_labels[i])])
+	# Plot our centroids as a black star.
 	for centroid in centroids:
 		plt.scatter(centroid[0], centroid[1], marker='*', c="#000000", s=100)
 	# show the plot
@@ -67,6 +76,7 @@ def onclick(event):
 		plot(data, centroids, labels, colors)
 
 def distance(pt1, pt2, axis=1):
+	# The usual distance formula.
 	return np.linalg.norm(pt2-pt1, axis=axis)
 
 def kmeans(k, data):
@@ -75,6 +85,7 @@ def kmeans(k, data):
 	centroid_y = np.random.choice(data[:,1], size=k)
 	centroid = np.array((centroid_x, centroid_y))
 
+	# Just to make things more readable
 	num_of_samples = len(data[:,0])
 	# Store the previous centroids
 	old_centroid = np.zeros(centroid.shape)
@@ -83,29 +94,45 @@ def kmeans(k, data):
 	# The distance between new and old centroid
 	dist = distance(centroid, old_centroid, axis=None)
 
-	# Loop until the distance is zero
+	# Loop until our centroid doesn't change
 	while dist != 0:
-		# assign each value to its closest cluster...
+		# for all of our samples
 		for i in range(num_of_samples):
+			# Get our distances for the current centroid
+			# and data.
 			distances = distance(data[i], centroid)
+			# get the indexes of the the least distances
 			clusters[i] = np.argmin(distances)
 		# Store the previous centroid
 		old_centroid = copy.deepcopy(centroid)
-		# finding the new centroid by taking the average value.
+		# finding the new centroid by taking the
+		# average value of our data if we're at the
+		# right cluster
 		for i in range(k):
 			centroid[i] = np.mean([data[j] for j in range(num_of_samples) if clusters[j] == i], axis=0)
+		# Did it change?
 		dist = distance(centroid, old_centroid, None)
+	# return the centroids and labels for our data
 	return centroid, clusters
 
 
+# Basically just a wrapper function around our version
+# of kmeans so that we don't have to change a lot of code.
 def cluster_the_data(data, k=2):
 	centroids, labels = kmeans(k=k, data=data)
 	return centroids, labels
 
+# Get n random colors.
+# We generally use this to color our clusters differently from
+# each other.
+# TODO: Force the colors to be different.
 def get_random_colors(n):
 	colors = np.zeros((1, n, 3))
 	for c in range(n):
 		colors[0, c] += (np.random.rand(), np.random.rand(), np.random.rand())
+	# Clip the colors between #111111 (dark gray) and #CCCCCC (light-ish gray)
+	# this is so that the colors are still visible on the graph
+	# but differ enough from the centroid plots.
 	return colors.clip(min=0x111111 / 0xFFFFFF, max = 0xCCCCCC / 0xFFFFFF)
 
 def main():
